73924f14fd971ee8515d8e47c453dabcc158085b
Ojo <ojobabajide2018@gmail.com>
2022-11-23 14:23:27 +0100

    route modified

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/index', [AdminController::class, 'index']);
+
+Route::get('/', [AdminController::class, 'index']);
